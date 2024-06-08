18be283eef3c1d12958af937166ac456b27fa4ee
Marcos Alejandro <115377268+MarcosAlejandroWeb@users.noreply.github.com>
2024-06-07 23:33:18 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const loginForm = document.getElementById('login-form');
+
+const userData = './data.json';
+
+
+loginForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+
+    const password = document.getElementById('password').value;
+
+
+    fetch(userData)
+
+        .then(response => response.json())
+
+        .then(data => {
+
+            const userFound = data.users.find(user => user.username === username && user.password === password);
+
+            if (userFound) {
+
+                window.location.href = 'welcome.html';
+
+            } else {
+
+                alert('Datos de inicio de sesión incorrectos');
+
+            }
+
+        });
+
+});
